18fd442f05e239c8165b9fc942a4c4a60db0ad55
Abhi-205 <abhayjadhav205@gmail.com>
2025-10-09 02:16:22 +0530

    Bit Manipulations

--- Bit/BitOperation.java
+++ Bit/BitOperation.java
@@ -38,6 +38,63 @@ public class BitOperation {
     return n & bitMask;
   }
 
+  public static int setLastIbits(int n, int i) {
+    int bitMask = ((1 << i) - 1);
+    return n | bitMask;
+  }
+
+  public static int clearRangeOfIBits(int n, int i, int j) {
+    int a = (~0) << (j + 1);
+    int b = (1 << i) - 1;
+    int bitMask = a | b;
+    return n & bitMask;
+  }
+
+  public static boolean isPowerOf2(int n) {
+    int bitMask = n & (n - 1);
+    if (bitMask == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public static int countSetBits(int n) {
+    int count = 0;
+    while (n > 0) {
+      if ((n & 1) != 0) {
+        count++;
+      }
+      n = n >> 1;
+    }
+    return count;
+  }
+
+  public static int fastExpo(int a, int b) {
+    int ans = 1;
+    while (b > 0) {
+      if ((b & 1) != 0) {
+        ans = ans * a;
+      }
+      a = a * a;
+      b = b >> 1;
+    }
+    return ans;
+  }
+
+  public static long fastModulo(long a, long b, long m) {
+    long ans = 1;
+    a %= m;
+    while (b > 0) {
+      if ((b & 1) != 0) {
+        ans = (ans * a) % m;
+      }
+      a = (a * a) % m;
+      b = b >> 1;
+    }
+    return ans;
+  }
+
   public static void main(String[] args) {
     // int num = 10;
     // int pos = 2;
@@ -54,5 +111,22 @@ public class BitOperation {
     int i = 2;
     int res = clearLastIbits(n, i);
     System.out.println(res);
+
+    // set Last i bits
+    int n1 = 12;
+    int i1 = 2;
+    int res1 = setLastIbits(n1, i1);
+    System.out.println(res1);
+
+    System.out.println(clearRangeOfIBits(31, 1, 3));
+
+    System.out.println(isPowerOf2(16));
+    System.out.println(isPowerOf2(17));
+
+    System.out.println(countSetBits(10));
+
+    System.out.println(fastExpo(13, 15));
+
+    System.out.println(fastModulo(5, 13, 3));
   }
 }

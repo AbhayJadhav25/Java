4b2df789aac9d2ffbfc4594d2254fc2ad23531cf
Abhi-205 <abhayjadhav205@gmail.com>
2025-12-24 17:16:15 +0530

    String Template Structure

---
+++ Java Practice/Basic/BasicX.java
@@ -0,0 +1,10 @@
+public class BasicX {
+  public static void main(String[] args) {
+    int age = 21;
+    String name = "Rahul";
+
+    System.out.println(STR."Hello , \{name} , You are \{age} years old");
+  }
+}
+
+// Not run , but there is concept of String Template Structur . like JavaScript.

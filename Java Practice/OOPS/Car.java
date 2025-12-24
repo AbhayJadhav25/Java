b4f7a9105b81ab12815c7fe8c249abdc24df5594
Abhi-205 <abhayjadhav205@gmail.com>
2025-12-25 02:37:57 +0530

    Class and object

---
+++ Java Practice/OOPS/Car.java
@@ -0,0 +1,9 @@
+public class Car {
+  String name;
+  String Color;
+  int speed;
+
+  public void drive() {
+    System.out.println(name + " Provides Speed " + speed + "km/h");
+  }
+}
---
+++ Java Practice/OOPS/CarX.java
@@ -0,0 +1,13 @@
+public class CarX {
+  public static void main(String[] args) {
+    Car c = new Car();
+    c.name = "KIA";
+    c.speed = 120;
+    c.drive();
+
+    Car c2 = new Car();
+    c2.name = "BMW";
+    c2.speed = 150;
+    c2.drive();
+  }
+}

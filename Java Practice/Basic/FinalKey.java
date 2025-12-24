d3038dc40557a5c9aa08f1137ce5d9fac1c1ca4f
Abhi-205 <abhayjadhav205@gmail.com>
2025-12-24 18:53:41 +0530

    Final Keyword

---
+++ Java Practice/Basic/FinalKey.java
@@ -0,0 +1,10 @@
+public class FinalKey {
+  public static void main(String[] args) {
+    final int a;
+    a = 10;
+    System.out.println(a);
+
+    final int b = 20;
+    System.out.println(b);
+  }
+}

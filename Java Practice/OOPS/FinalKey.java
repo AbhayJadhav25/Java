53641e252a73d255265a937cd8b89da77d6a6fcc
Abhi-205 <abhayjadhav205@gmail.com>
2026-01-08 00:48:43 +0530

    No commit

---
+++ Java Practice/OOPS/FinalKey.java
@@ -0,0 +1,7 @@
+public class FinalKey {
+  public static void main(String[] args) {
+    final int a = 20;
+    System.out.println(a);
+    // a = 10; //error: cannot assign a value to final variable a
+  }
+}
